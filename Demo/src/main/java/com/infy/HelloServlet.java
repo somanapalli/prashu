ab05cf4262b1f7a0f149931cf81f130df7d74410
somanapalli <rams.11mca@gmail.com>
2022-04-08 20:05:22 +0530

    commited

---
+++ Demo/src/main/java/com/infy/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.infy;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class HelloServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		pw.println("welcome to servlets");
+		}
+
+}
